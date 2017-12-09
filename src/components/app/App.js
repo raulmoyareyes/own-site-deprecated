80c7694e64e7c788838fc22118e41de52862ea1b
Raúl Moya Reyes <info@raulmoya.es>
2017-12-09 20:10:16 +0100

    Add some components to create deploy flow

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from '../header'
+
+import './assets/app.css'
+
+const App = () => {
+    return (
+        <Header />
+    )
+}
+
+export default App
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './assets/header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        raúl<span className="header__title--effect">:moya</span>
+      </h1>
+      <h2 className="header__subtitle">
+        Frontend Engineer & Javascript Developer
+      </h2>
+    </header>
+  )
+}
+
+export default Header
