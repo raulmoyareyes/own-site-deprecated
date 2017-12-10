fcfca2bb41c66131df1ffd037c0e88b5dc3f64ac
Raúl Moya Reyes <info@raulmoya.es>
2017-12-10 14:18:05 +0100

    Feature/sections with data (#13)
    * Sections with data to test components with selectable theme
    
    * Isolate jest config from package.json

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Section = () => {
+const Section = ({ title, subtitle }) => {
   return (
-    <section>
-      Section
+    <section className="section">
+      <h3>{ title }</h3>
+      <h4>{ subtitle }</h4>
     </section>
   )
+}
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 }
 
 export default Section
--- src/components/section/tests/Section.test.js
+++ src/components/section/tests/Section.test.js
@@ -4,10 +4,17 @@ import { shallow } from 'enzyme'
 import Section from '../'
 
 describe('<Header />', function () {
+  const props = {
+    id: 1,
+    title: 'GOOD TITLE',
+    subtitle: 'the best subtitle',
+    theme: 1,
+  }
+
   let section
 
   beforeEach(function () {
-    section = shallow(<Section />)
+    section = shallow(<Section { ...props }/>)
   })
 
   it('should render correctly', function () {
