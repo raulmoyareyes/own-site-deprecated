dfe159b7ade02892f5b20ca5035766dab244992c
Raúl Moya Reyes <info@raulmoya.es>
2017-12-10 13:32:40 +0100

    Sections with data to test components with selectable theme

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,18 @@ import React, { Fragment } from 'react'
 import Header from '../header'
 import Section from '../section'
 
+// TODO: Replace with Firebase Database
+import SECTIONS from '../section/datas/sections'
+
 import './assets/app.css'
 
 const App = () => {
   return (
     <Fragment>
       <Header />
-      <Section />
+      { SECTIONS.map( section =>
+        <Section key={ section.id } { ...section } />
+      ) }
     </Fragment>
   )
 }
---
+++ src/components/section/datas/sections.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    title: 'SOBRE MI',
+    subtitle: 'Una pequeña introducción para conocernos mejor',
+    theme: 1,
+  },
+  {
+    id: 2,
+    title: "EXPERIENCIA LABORAL",
+    subtitle: "Mis puestos anteriores",
+    theme: 2,
+  }
+]
