5a72208ca784ab7913ec2e2052485ea6e807649e
Raúl Moya Reyes <info@raulmoya.es>
2018-01-02 00:08:30 +0100

    Update config (#33)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/app'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './services/service-worker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
