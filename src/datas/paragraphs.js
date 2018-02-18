688e68638a19113f4e1b0462b81a5ae0e7d3a6ac
Raúl Moya Reyes <info@raulmoya.es>
2018-02-18 01:04:56 +0100

    Add custom favicon and fix some styles (#79)

--- src/datas/paragraphs.js
+++ src/datas/paragraphs.js
@@ -3,7 +3,7 @@ export default {
     "text": "Andaluz, explorador en el mundo del desarrollo web y amante de Javascript. Disfruto creando aplicaciones y trabajo a diario con la mayoría de las tecnologías que se utilizan con este lenguaje. En los últimos tiempos, React y la programación funcional me entusiasman."
   },
   '2': {
-    "text": "Durante mi carrera, he acabado liderando algún equipo de Front-end autogestionado con metodologías ágiles, mejorando los procesos de desarrollo pero sin dejar de programar que me encanta. Tengo sólidos conocimientos en ingeniería de software y en gestión de proyectos por lo que suelo ayudar a solventar los problemas que surgen en el día a día."
+    "text": "Durante mi carrera, he acabado liderando algún equipo de Front-end autogestionado con metodologías ágiles y mejorando los procesos de desarrollo, siempre sin dejar de programar que es lo que me encanta. Tengo sólidos conocimientos en ingeniería de software y en gestión de proyectos por lo que suelo ayudar a solventar los problemas que surgen en el día a día."
   },
   '3': {
     "text": "El crecimiento personal dentro de los proyectos es lo que más me motiva y me hace sacar lo mejor de mí. Estoy acostumbrado a trabajar en equipo y a compartir conocimiento con los demás. Me gusta el mundo startup y he trabajado en varias de ellas. Es un modelo de empresa que pese a las dificultades que supone arrancar proyectos desde cero, ofrece grandes retos a todos los niveles. Ver cómo crece un proyecto es algo muy satisfactorio."
