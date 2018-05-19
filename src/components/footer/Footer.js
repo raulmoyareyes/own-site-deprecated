6aa98645712ab1bd1566017e9652affe62866d07
Raúl Moya Reyes <info@raulmoya.es>
2018-05-19 20:09:00 +0200

    Feature/#44 (#177)
    * Add icon component and social network
    
    * Add icon tests

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
+import { Icon } from '../icon'
+
 import './assets/footer.css'
 
-const WEBSITE_NAME = process.env.REACT_APP_WEBSITE_NAME
+const WEBSITE_AUTHOR = process.env.REACT_APP_WEBSITE_AUTHOR
 const YEAR = new Date().getFullYear()
+
+const SOCIAL_NETWORKS = [
+  { name: 'github', link: 'https://github.com/raulmoyareyes' },
+  { name: 'linkedin', link: 'https://www.linkedin.com/in/raulmoyareyes/' },
+  { name: 'twitter', link: 'https://twitter.com/raulmoyareyes' },
+]
 
 export const Footer = () => (
   <div className="footer">
     <div className="footer__content">
-      Copyright © { YEAR } - { WEBSITE_NAME }
+      <span>Copyright © { YEAR } - { WEBSITE_AUTHOR }</span>
+
+      { SOCIAL_NETWORKS.map((network, index) => (
+        <a key={ index } href={ network.link } target="_blank" rel="noopener noreferrer">
+          <Icon name={ network.name }/>
+        </a>
+      ))}
     </div>
   </div>
 )
